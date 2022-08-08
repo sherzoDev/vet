b6ef55a83d2837aed8d911c09beb0ff51431fdf3
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-08-08 22:27:38 +0500

    Add New Modal

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ var elOverlay = document.querySelector(".overly");
 var elOverlayModal = document.querySelector(".modal");
 var elCloseModalBtn = document.querySelector(".js-modal-close");
 
-elOpenModalBtn.addEventListener("click" , function(){
-   elOverlay.classList.add("modal--open");
+elOpenModalBtn.addEventListener("click", function () {
+    elOverlay.classList.add("modal--open");
 })
 
-elCloseModalBtn.addEventListener("click" , function(){
-   elOverlay.classList.remove("modal--open");
+elCloseModalBtn.addEventListener("click", function () {
+    elOverlay.classList.remove("modal--open");
 })
